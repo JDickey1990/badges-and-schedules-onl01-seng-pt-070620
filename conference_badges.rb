88fce6ed7ce2277005c6ac70c9a2d9f7fd3b6f59
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-11 21:35:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ batch_badge_creator(["Edsger","Ada","Charles","Alan","Grace","Linus,Matz"])
 def assign_rooms(speakers)
   room_assignments=[]
   speakers.each_with_index do |name,index|
-    room_assignments<< "Hello, #{name}!You'll be assigned to room #{index +=1}!"
+    room_assignments<< "Hello, #{name}! You'll be assigned to room #{index +=1}!"
  end
 return room_assignments
 end
