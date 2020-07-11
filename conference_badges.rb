1539f97c0913f09b10acd60f7a1cea20b053aded
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-11 20:36:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 badge_maker("Arel")
+
+def batch_badge_creator(badge_names)
+  badge_messages=[]
+  badge_names.
+end
+end
+batch_badge_creator([Edsger,Ada,Charles,Alan,Grace,Linus,Matz])
